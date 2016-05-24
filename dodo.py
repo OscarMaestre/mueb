11fc7cb4fcbf5fa01f26cddfe8f540be16b59923
Oscar G.G <profesor.oscar.gomez@gmail.com>
2016-05-24 08:57:26 +0200

    Nueva carga

--- dodo.py
+++ dodo.py
@@ -2,29 +2,43 @@
 #coding=utf-8
 
 from utilidades.ficheros.GestorFicheros import GestorFicheros
-import glob, platform
+import glob, platform, sys, os
+
+from datetime import date
+
+hoy=date.today()
+fecha_hoy=hoy.strftime("%Y%m%d")
+print (fecha_hoy)
 
 
 gf=GestorFicheros()
 
-patrones=["al/*.html"]
+patrones=["al/*.html", "to/*.html"]
 
 for p in patrones:
     ficheros_a_borrar=glob.glob(p)
     for f in ficheros_a_borrar:
         gf.borrar_fichero ( f )
         
+FICHERO_RESULTADO="resultados" + os.sep +"resultados_{0}.html".format(fecha_hoy)
+
 if platform.system()=="Linux":
-    DESCARGADOR         =   "./descargador_html_al.py"
-    PROCESAR_ID         =   "./procesar_id.py"
+    DESCARGADOR_AL      =   "./descargador_html_al.py"
+    DESCARGADOR_TO      =   "./descargador_html_to.py"
+    PROCESAR_ID_AL      =   "./procesar_id.py"
+    PROCESAR_ID_TO      =   "./procesar_to.py"
     CREAR_ESTADISTICAS  =   "./crear_estadisticas.py"
 else:
-    DESCARGADOR         =   "descargador_html_al.py"
-    PROCESAR_ID         =   "procesar_id.py"
+    DESCARGADOR_AL      =   "descargador_html_al.py"
+    DESCARGADOR_TO      =   "descargador_html_to.py"
+    PROCESAR_ID_AL      =   "procesar_id.py"
+    PROCESAR_ID_TO      =   "procesar_to.py"
     CREAR_ESTADISTICAS  =   "crear_estadisticas.py"
     
     
-gf.ejecutar_comando ( DESCARGADOR , "")
-gf.ejecutar_comando ( PROCESAR_ID , "")
-gf.ejecutar_comando ( CREAR_ESTADISTICAS , ">", "resultados.html")
+gf.ejecutar_comando ( DESCARGADOR_AL , "")
+gf.ejecutar_comando ( DESCARGADOR_TO , "")
+gf.ejecutar_comando ( PROCESAR_ID_AL , "")
+gf.ejecutar_comando ( PROCESAR_ID_TO , "")
+gf.ejecutar_comando ( CREAR_ESTADISTICAS , ">", FICHERO_RESULTADO)
 
